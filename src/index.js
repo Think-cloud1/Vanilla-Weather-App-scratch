750eb85e1b7bbc604bc72800528e858a2e67f51a
Think-cloud1 <tinkova90@gmail.com>
2025-01-19 09:42:12 +0000

    adding axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+function refreshWeather(response){
+     let temperatureElement = document.querySelector("#temperature");
+     let temperature = response.data.temperature.current;
+     let cityElement = document.querySelector("#city");
+
+     cityElement.innerHTML = response.data.city;
+     temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+
+function searchCity (city){
+    let apiKey = "3c3a51f78ta373d8e38408fe6b4c8bo0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
